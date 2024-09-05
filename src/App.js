a50163dc874549425d6c029d4b38cf872a554a12
artem <vadis1766@gmail.com>
2024-09-05 10:59:06 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -3,39 +3,20 @@ import './styles/App.css';
 import WalletInfo from './components/WalletInfo';
 import QuestList from './components/QuestList';
 import TonConnect from '@tonconnect/sdk';
-import { SDKProvider, useMainButton, useInitData, useLaunchParams } from '@telegram-apps/sdk-react';
 
 const tonconnect = new TonConnect();
 
-// Моковые данные для локальной разработки
-const mockLaunchParams = {
-  start_param: 'test_param'
-};
-
-const mockInitData = {
-  user: {
-    id: 123456789,
-    username: 'test_user',
-    language_code: 'en'
-  }
-};
-
-function AppContent() {
+function App() {
   const [walletAddress, setWalletAddress] = useState('');
   const [tokenBalance, setTokenBalance] = useState(0);
   const [referralCount, setReferralCount] = useState(0);
-  const [isLocalDevelopment, setIsLocalDevelopment] = useState(false);
-
-  // Используем реальные данные в Telegram и моковые при локальной разработке
-  const mainButton = useMainButton(isLocalDevelopment);
-  const initData = useInitData(isLocalDevelopment ? mockInitData : undefined);
-  const launchParams = useLaunchParams(isLocalDevelopment ? mockLaunchParams : undefined);
+  const [isTelegramWebAppReady, setIsTelegramWebAppReady] = useState(false);
 
   useEffect(() => {
-    // Проверяем, запущено ли приложение в среде Telegram
-    if (!window.Telegram?.WebApp) {
-      console.log('Running in local development mode');
-      setIsLocalDevelopment(true);
+    const tgWebApp = window.Telegram?.WebApp;
+    if (tgWebApp) {
+      tgWebApp.ready();
+      setIsTelegramWebAppReady(true);
     }
 
     const connectWallet = async () => {
@@ -52,25 +33,29 @@ function AppContent() {
 
     connectWallet();
 
-    if (launchParams.start_param) {
-      console.log('App launched with start_param:', launchParams.start_param);
-    }
-
-    if (mainButton) {
-      mainButton.setText('Start');
-      mainButton.show();
-      mainButton.onClick(handleCreateUser);
+    if (tgWebApp) {
+      tgWebApp.MainButton.setText('Start');
+      tgWebApp.MainButton.show();
+      tgWebApp.MainButton.onClick(handleCreateUser);
     }
 
     return () => {
-      mainButton?.hide();
-      mainButton?.offClick(handleCreateUser);
+      if (tgWebApp) {
+        tgWebApp.MainButton.offClick(handleCreateUser);
+      }
     };
-  }, [mainButton, launchParams]);
+  }, []);
 
   const handleCreateUser = async () => {
-    if (!initData) {
-      console.error('Init data is not available');
+    const tgWebApp = window.Telegram?.WebApp;
+    if (!tgWebApp) {
+      console.error('Telegram WebApp is not available');
+      return;
+    }
+
+    const initData = tgWebApp.initDataUnsafe;
+    if (!initData || !initData.user) {
+      console.error('User data is not available');
       return;
     }
 
@@ -81,15 +66,15 @@ function AppContent() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          telegramId: initData.user?.id,
-          username: initData.user?.username,
+          telegramId: initData.user.id,
+          username: initData.user.username,
           walletAddress: walletAddress,
         }),
       });
 
       if (response.ok) {
         console.log('User successfully created');
-        mainButton?.hide();
+        tgWebApp.MainButton.hide();
       } else {
         console.error('Error creating user');
       }
@@ -100,27 +85,25 @@ function AppContent() {
 
   return (
     <div className="app-container">
-      <div className="total-balance">
-        <span className="balance-value">{tokenBalance}</span>
-        <span className="balance-currency">TON</span>
-      </div>
-      <img className="app-image" src="ball1.png" alt="App Logo" />
-      <div className="wallet-info-container">
-        <WalletInfo address={walletAddress} />
-      </div>
-      <QuestList />
-      <div className="referral-info">
-        Приглашено рефералов: {referralCount}
-      </div>
+      {isTelegramWebAppReady ? (
+        <>
+          <div className="total-balance">
+            <span className="balance-value">{tokenBalance}</span>
+            <span className="balance-currency">TON</span>
+          </div>
+          <img className="app-image" src="ball1.png" alt="App Logo" />
+          <div className="wallet-info-container">
+            <WalletInfo address={walletAddress} />
+          </div>
+          <QuestList />
+          <div className="referral-info">
+            Приглашено рефералов: {referralCount}
+          </div>
+        </>
+      ) : (
+        <div>Loading Telegram WebApp...</div>
+      )}
     </div>
-  );
-}
-
-function App() {
-  return (
-    <SDKProvider>
-      <AppContent />
-    </SDKProvider>
   );
 }
 
