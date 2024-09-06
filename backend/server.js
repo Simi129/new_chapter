5bd5ed2e8953d437409bf861782f20b71b81bad0
artem <vadis1766@gmail.com>
2024-09-06 20:10:20 +0300

    asdr

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { sequelize } = require('./src/models');
 const userRoutes = require('./src/routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Настройка CORS
+app.use(cors({
+  origin: ['https://simi129.github.io', 'http://localhost:3000' , 'https://1349-78-84-19-24.ngrok-free.app'], // Добавьте все нужные домены
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-telegram-bot-token']
+}));
 
 app.use(express.json());
 
@@ -17,7 +25,11 @@ const checkBotToken = (req, res, next) => {
   next();
 };
 
+// Применяем middleware checkBotToken только к маршрутам /api/users
 app.use('/api/users', checkBotToken, userRoutes);
+
+// Обработка префлайт-запросов для всех маршрутов
+app.options('*', cors());
 
 sequelize.sync({ force: false })
   .then(() => {
